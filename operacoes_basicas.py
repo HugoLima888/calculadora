5bd030d24963abe5a70b6cdf156e48c8569085da
Hugo Franco Rodrigues de Lima <hugo.lima@bravolog.com.br>
2022-01-06 00:13:09 -0300

    interacao usuario - encap

--- operacoes_basicas.py
+++ operacoes_basicas.py
@@ -1,29 +1,47 @@
 # Operações realizadas pela calculadora #
 
+class Operacoes:
+    def __init__(self):
+        self.seleciona_opcao()
 
-def soma():
-    n1 = int(input('Informe um valor: '))
-    n2 = int(input('Informe um valor: '))
-    resultado = n1 + n2
-    print(f'A soma dos valores é: {resultado}')
+    def __int__(self):
+        pass
 
+    def seleciona_opcao(self):
+        self.opcao = int(input('Escolha uma das opções disponíveis 1 - Soma, 2: Subtração,'
+                               '3: Multiplicação, 4: Divisão - : '))
+        self.operacao = self.opcao
+        if self.operacao == 1:
+            self.soma()
+        elif self.operacao == 2:
+            self.subt()
+        elif self.operacao == 3:
+            self.multi()
+        elif self.operacao == 4:
+            self.divi()
+        else:
+            raise ValueError('Operação inválida!!')
 
-def subt():
-    n1 = int(input('Informe um valor: '))
-    n2 = int(input('Informe um valor: '))
-    resultado = n1 - n2
-    print(f'A subtração dos valores é: {resultado}')
+    def soma(self):
+        n1 = int(input('Informe um valor: '))
+        n2 = int(input('Informe um valor: '))
+        resultado = n1 + n2
+        print(f'A subtração dos valores é: {resultado}')
 
+    def subt(self):
+        n1 = int(input('Informe um valor: '))
+        n2 = int(input('Informe um valor: '))
+        resultado = n1 - n2
+        print(f'A subtração dos valores é: {resultado}')
 
-def multi():
-    n1 = int(input('Informe um valor: '))
-    n2 = int(input('Informe um valor: '))
-    resultado = n1 * n2
-    print(f'A multiplicação dos valores é: {resultado}')
+    def multi(self):
+        n1 = int(input('Informe um valor: '))
+        n2 = int(input('Informe um valor: '))
+        resultado = n1 * n2
+        print(f'A multiplicação dos valores é: {resultado}')
 
-
-def divi():
-    n1 = int(input('Informe um valor: '))
-    n2 = int(input('Informe um valor: '))
-    resultado = n1 / n2
-    print(f'A multiplicação dos valores é: {round(resultado, 2)}')
+    def divi(self):
+        n1 = int(input('Informe um valor: '))
+        n2 = int(input('Informe um valor: '))
+        resultado = n1 / n2
+        print(f'A multiplicação dos valores é: {round(resultado, 2)}')
